9b1683fd298542466529a2d85eff13a0d9b654c1
Zachary <zacharyblundell@live.com>
2025-08-18 09:55:08 +0200

    added filler info

--- app/(public)/[locale]/associations/[associationName]/articles/[articleSlug]/page.tsx
+++ app/(public)/[locale]/associations/[associationName]/articles/[articleSlug]/page.tsx
@@ -4,9 +4,17 @@
 export default async function articlePage({
   params,
 }: {
-  params: Promise<{ articleSlug: string }>;
+  params: Promise<{ associationName: string; articleSlug: string }>;
 }) {
-  const { articleSlug } = await params;
+  const { associationName, articleSlug } = await params;
 
-  return <div>{articleSlug}</div>;
+  return (
+    <div>
+      <h1>{articleSlug}</h1>
+      <p>
+        This is an example of the article page for the association{" "}
+        {associationName}. Here you would see the rendered markdown
+      </p>
+    </div>
+  );
 }
--- app/(public)/[locale]/associations/[associationName]/articles/page.tsx
+++ app/(public)/[locale]/associations/[associationName]/articles/page.tsx
@@ -1,12 +1,21 @@
 //[local]/associations/[associationName]/articles
 //Articles-page
 
-export default function AssociationArticlesPage() {
+export default async function AssociationArticlesPage({
+  params,
+}: {
+  params: Promise<{ associationName: string }>;
+}) {
+  const { associationName } = await params;
+
   return (
     <section className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
-      <h1 className="text-2xl font-semibold">Articles for association </h1>
+      <h1 className="text-2xl font-semibold">
+        Articles for association {associationName}{" "}
+      </h1>
       <p className="mt-2 text-gray-600">
-        Route to demonstrate navigation and active link styling.
+        Route to demonstrate the list of articles for a specific association
+        called: {associationName}
       </p>
     </section>
   );
