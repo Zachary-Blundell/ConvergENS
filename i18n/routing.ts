ebf39f5ad3c9e7ac7cc78fd61c7a7315e353b2a9
Zachary Blundell <zacharyblundell@live.com>
2025-08-22 12:14:47 +0200

    updated to fr default

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "fr"],
-  defaultLocale: "en",
+  defaultLocale: "fr",
   // localePrefix: 'as-needed'
   // This will make it so that the locale is not prefixed in the URL if it is the default locale.
 });
