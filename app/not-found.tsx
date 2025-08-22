67c210d0922c72790f1ea159adadcbd4424c9bfb
Zachary Blundell <zacharyblundell@live.com>
2025-08-22 12:13:55 +0200

    base not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,77 @@
+import { useTranslations } from "next-intl";
+import "@/styles/globals.css";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function NotFound() {
-  return <div>404 Not found</div>;
+  const t = useTranslations("404");
+
+  return (
+    <section className="bg-white dark:bg-black text-black dark:text-white">
+      <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
+        <div className="flex flex-col items-center max-w-sm mx-auto text-center">
+          <p
+            className="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800"
+            aria-label={t("badgeLabel")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+              />
+            </svg>
+          </p>
+
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+            {t("title")}
+          </h1>
+
+          <p className="mt-4 text-gray-500 dark:text-gray-400">
+            {t("description")}
+          </p>
+
+          <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+            <Link
+              href=".."
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:scale-105  dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 rtl:rotate-180"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
+              <span>{t("goBack")}</span>
+            </Link>
+
+            <Link
+              href="/"
+              className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-black rounded-lg shrink-0 sm:w-auto hover:scale-105 hover:bg-gray-900 dark:hover:bg-gray-100 dark:bg-black focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-grey-500"
+            >
+              {t("home")}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
