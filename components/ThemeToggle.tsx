1fdf81677f53109db06dea6b469683526ab39d71
Zachary Blundell <zacharyblundell@live.com>
2025-08-22 12:14:29 +0200

    theme toggle created

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      className="w-10 h-10 p-0 rounded-full"
+      onClick={() => {
+        setTheme(theme === "light" ? "dark" : "light");
+      }}
+    >
+      <FaSun className="absolute h-10 w-10 rotate-0 scale-100 dark:rotate-90 dark:scale-0"></FaSun>
+      <FaMoon className="absolute h-10 w-10 rotate-90 scale-0 dark:rotate-0 dark:scale-100"></FaMoon>
+    </Button>
+  );
+}
