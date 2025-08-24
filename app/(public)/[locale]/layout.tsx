a0c76a0ddf14e32572058131c8b3673c5ab93bb7
Zachary <zacharyblundell@live.com>
2025-08-24 13:13:01 +0200

    navbar rework

---
+++ app/(public)/[locale]/layout.tsx
@@ -0,0 +1,35 @@
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { routing } from "@/i18n/routing";
+import { notFound } from "next/navigation";
+import NavBar from "@/components/NavBar";
+
+export const metadata: Metadata = {
+  title: "YourApp",
+  description: "A crisp Next.js + Tailwind starter",
+};
+
+export default function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const { locale } = params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  console.log("Locale:", locale);
+  console.log("Locales in routing:", routing.locales);
+  return (
+    <main id="main" className="flex-1">
+      <NextIntlClientProvider>
+        <NavBar />
+        {children}
+      </NextIntlClientProvider>
+    </main>
+  );
+}
