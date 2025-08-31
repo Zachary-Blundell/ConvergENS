76bcebdc5b46638edb4874fff5c4e434150c8277
Zachary <zacharyblundell@live.com>
2025-08-31 18:59:13 +0200

    allow other routes
    favicon robots sitemap admin dashboard login reset

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,6 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  matcher:
+    "/((?!api|_next|.*\\..*|favicon.ico|robots.txt|sitemap.xml|admin|dashboard|login|reset).*)",
 };
