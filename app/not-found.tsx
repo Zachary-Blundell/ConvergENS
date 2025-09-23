cdd8736d84010720b74567998e3f838c32bfca0a
Zachary <zacharyblundell@live.com>
2025-09-23 19:32:45 +0200

    style update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,8 +9,10 @@ export default function NotFound() {
     <section className="bg-white dark:bg-black text-black dark:text-white">
       <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
         <div className="flex flex-col items-center max-w-sm mx-auto text-center">
+          <h2 className="m-3 text-5xl">404</h2>
+
           <p
-            className="p-3 text-sm font-medium text-highlight rounded-full bg-blue-50 dark:bg-gray-800"
+            className="p-3 text-sm font-medium text-highlight rounded-full bg-blue-100 dark:bg-gray-800"
             aria-label={t("badgeLabel")}
           >
             <svg
@@ -30,37 +32,13 @@ export default function NotFound() {
             </svg>
           </p>
 
-          <h1 className="mt-3 text-2xl text-gray-800 dark:text-white md:text-3xl">
+          <h1 className="mt-3 text-2xl text-fg-primary md:text-3xl">
             {t("title")}
           </h1>
 
-          <p className="mt-4 text-gray-500 dark:text-gray-400">
-            {t("description")}
-          </p>
+          <p className="mt-4 text-fg-muted">{t("description")}</p>
 
           <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
-            <Link
-              href=".."
-              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:scale-105  dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5 rtl:rotate-180"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-                />
-              </svg>
-              <span>{t("goBack")}</span>
-            </Link>
-
             <Link
               href="/"
               className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-black rounded-lg shrink-0 sm:w-auto hover:scale-105 hover:bg-gray-900 dark:hover:bg-gray-100 dark:bg-black focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-grey-500"
