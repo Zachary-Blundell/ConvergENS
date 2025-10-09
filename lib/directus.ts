ad35f50f20c8b6bf8cf503fb73bc433a67307e25
Zachary <zacharyblundell@live.com>
2025-10-09 18:58:10 +0200

    single quotes

--- lib/directus.ts
+++ lib/directus.ts
@@ -1,7 +1,7 @@
-import { createDirectus, rest } from "@directus/sdk";
+import { createDirectus, rest } from '@directus/sdk';
 
 export const directus = createDirectus(process.env.DIRECTUS_API_ENDPOINT).with(
   rest({
-    onRequest: (options) => ({ ...options, cache: "no-store" }),
+    onRequest: (options) => ({ ...options, cache: 'no-store' }),
   }),
 );
