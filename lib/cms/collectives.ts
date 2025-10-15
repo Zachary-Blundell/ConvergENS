39af47399122ab972c1427f4f8d1189e27a7d906
Zachary <zacharyblundell@live.com>
2025-10-15 15:46:52 +0200

    update log

--- lib/cms/collectives.ts
+++ lib/cms/collectives.ts
@@ -185,7 +185,6 @@ export async function getOrganisationCards(
       description: collectiveTr?.description ?? null,
       socials: i.socials ?? [],
     };
-    console.log(result);
     return result;
   });
 }
