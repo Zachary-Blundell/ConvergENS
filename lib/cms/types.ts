97559ac3a7c91269086dfedcd0c446c2b9323365
Zachary <zacharyblundell@live.com>
2025-12-23 11:30:23 +0100

    moved collective ui here

--- lib/cms/types.ts
+++ lib/cms/types.ts
@@ -5,6 +5,15 @@ export type MaybeExpanded<T> = Id | T;
 
 export type DirectusImage = {
   id: string;
+  description?: string | null;
   width?: number | null;
   height?: number | null;
+};
+
+export type CollectiveForUIRaw = {
+  id: Id;
+  name?: string | null;
+  slug?: string | null;
+  color?: string | null;
+  logo?: DirectusImage | null;
 };
