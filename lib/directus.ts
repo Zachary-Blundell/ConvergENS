02c8d151a2cec883fec969e5905c26f384912c12
Zachary Blundell <zacharyblundell@live.com>
2026-03-29 13:48:07 +0200

    removed spaces

--- lib/directus.ts
+++ lib/directus.ts
@@ -1,7 +1,5 @@
 import { createDirectus, rest } from '@directus/sdk';
 
-
-
 console.log("DIRECTUS_API_ENDPOINT", process.env.DIRECTUS_API_ENDPOINT);
 
 export const directus = createDirectus(process.env.DIRECTUS_API_ENDPOINT).with(
